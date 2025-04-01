8d8cde0a26343a50f894ce4a78abd78716249945
joseCalfueque <j.calfueque02@ufromail.cl>
2025-04-01 00:34:15 -0300

    agrego lo que vendria siendo el "perfil" del o la paciente

---
+++ src/Paciente.java
@@ -0,0 +1,11 @@
+public class Paciente {
+    private String nombre;
+    private int edad;
+    private String contacto;
+
+    public Paciente(String nombre, int edad, String contacto) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.contacto = contacto;
+    }
+}
