1205129cb87ea4dfd21f562b539fb3478ef8d5e5
joseCalfueque <j.calfueque02@ufromail.cl>
2025-04-01 00:32:27 -0300

    agrego lo que vendria siendo el "perfil" del medico

---
+++ src/Medico.java
@@ -0,0 +1,11 @@
+public class Medico {
+    private String nombre;
+    private String especialidad;
+    private int experiencia;
+
+    public Medico(String nombre, String especialidad, int experiencia) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.experiencia = experiencia;
+    }
+}
